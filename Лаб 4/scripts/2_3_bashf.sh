2207ac4e157ce6c68f2d5e2b763f1e545efd9fce
vladoslavandos <Vlad.Dorofeev.b19-515@yandex.ru>
2021-11-27 16:32:51 +0300

    2.3 - строки

---
+++ Лаб 4/scripts/2_3_bashf.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd ~
+cat bash.txt | grep "000000" > /tmp/zeros
+cat bash.txt | grep -v "000000" > /tmp/nozeros
+head -10 /tmp/zeros
+tail -10 /tmp/zeros
+head -10 /tmp/nozeros
+tail -10 /tmp/nozeros
